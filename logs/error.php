9d915e7af8b5eee1b0503b31476b7573a4d50b4b
eliska <eliska@luna-B400A.(none)>
2015-05-24 21:57:44 +0200

    first

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-05-11 11:06:56 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-05-11T11:06:56+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
